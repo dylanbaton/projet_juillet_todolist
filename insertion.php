7b4bfe3812951247107c9a04f57ce2ce8b3278f8
dylanbaton <dylan.baton@outlook.fr>
2024-07-16 13:28:06 +0200

    sauvegarde avant changement du code

--- insertion.php
+++ insertion.php
@@ -11,3 +11,17 @@ $execution=$bdd->exec($query);
 header('Location: index.php');
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div>
+    <input type="checkbox" id="nom" name="nom" checked />
+  </div>
+</body>
+</html>
